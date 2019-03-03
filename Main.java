005eec749b84b1f0699c1305a1db025025889aab
shaitanshamma <kurbatov_andr@hotmail.com>
2019-03-03 18:25:35 +0300

    Финальная версия

--- Main.java
+++ Main.java
@@ -3,23 +3,30 @@ package ru.geekbrains.lesson_3;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Ваша задача угадать число:");
-        int range = 100;
-        int number = (int)(Math.random() * range);
-        while (true){
+        for (int i = 10; i <= 30; i+=10) playerLevel(i);
+        System.out.println("Вы выиграли!!!");
+        scanner.close();
+    }
+
+    private static void playerLevel(int range) {
+        int number = (int) (Math.random() * range);
+        while (true) {
             System.out.println("Угадайте чилсо от 0 до " + range);
             int new_numb = scanner.nextInt();
-            if (number == new_numb){
+            if (number == new_numb) {
                 System.out.println("Ай красавчик!");
                 break;
-            } else if (new_numb < number){
+            } else if (new_numb < number) {
                 System.out.println("Загаданное число больше");
             } else {
                 System.out.println("Загаданное число меньше");
             }
-        } scanner.close();
+        }
     }
 }
+
